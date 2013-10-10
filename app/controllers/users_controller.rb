aeb809b73d57150c102982d30adb4fc3d416dfc4
breenusie <bryan.j.kelly+1@gmail.com>
2013-10-10 15:55:17 +0100

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
+  before_action :already_signed_in,   only: [:new, :create]
 
 def index
     @users = User.paginate(page: params[:page])
@@ -9,6 +10,7 @@ def index
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page], per_page: 15)
   end	
   
   def new
@@ -23,6 +25,7 @@ def index
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
+      flash[:error] = "Error, you done fucked up!"
       render 'new'
     end
   end
@@ -35,6 +38,7 @@ def index
       flash[:success] = "Profile updated"
       redirect_to @user
     else
+      flash[:error] = "Error, you done fucked up!"
       render 'edit'
     end
   end
@@ -53,13 +57,6 @@ def index
 
     # Before filters
 
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
-
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
@@ -67,5 +64,11 @@ def index
 
     def admin_user
       redirect_to(root_url) unless current_user.admin?
+    end
+
+    def already_signed_in
+      if signed_in?
+        redirect_to(root_url) 
+      end
     end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -47,7 +47,26 @@ describe "Authentication" do
   describe "authorization" do
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
+      
+      describe "should not have signed in links" do
+        it { should_not have_link('Profile',     href: user_path(user)) }
+        it { should_not have_link('Settings',    href: edit_user_path(user)) }
+        it { should_not have_link('Sign out',    href: signout_path) }
+      end
+      
+      describe "in the Microposts controller" do
 
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
+      
       describe "in the Users controller" do
 
         describe "visiting the edit page" do
@@ -78,6 +97,20 @@ describe "Authentication" do
 
           it "should render the desired protected page" do
             expect(page).to have_title('Edit user')
+          end
+          
+          describe "when signing in again" do
+            before do
+              delete signout_path
+              visit signin_path
+              fill_in "Email",    with: user.email
+              fill_in "Password", with: user.password
+              click_button "Sign in"
+            end
+
+            it "should render the default (profile) page" do
+              expect(page).to have_title(user.name)
+            end
           end
         end
       end
